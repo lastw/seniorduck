aa7e5bd3737348e6df981150bc6a0381244ba384
Vlad Sapozhnikov <me@lastw.net>
2022-11-17 11:18:50 +0700

    final

--- src/screens/duck.tsx
+++ src/screens/duck.tsx
@@ -4,34 +4,61 @@ import { Message } from '../components/message';
 
 export const DuckScreen = () => {
   const [log, setLog] = useState<Array<{ type: 'from' | 'to'; text: string }>>([]);
-  // const [isTyping, setIsTyping] = useState(false);
+  const [isTyping, setIsTyping] = useState(false);
 
-  const timeoutRef = useRef<any>();
+  const typingTimeoutRef = useRef<any>();
+  const messageTimeoutRef = useRef<any>();
+
+  const startTyping = () => {
+    setIsTyping(true);
+
+    clearTimeout(messageTimeoutRef.current);
+
+    // интервал между началом печати и отправкой ответа
+    messageTimeoutRef.current = setTimeout(() => {
+      sendResponse();
+    }, 1000 + Math.floor(Math.random() * 3000));
+  };
+
+  const sendResponse = () => {
+    setIsTyping(false);
+    setLog((log) => [...log, { text: getSeniorMessage(), type: 'to' }]);
+  };
 
   const handleSubmit = useCallback((message: string) => {
     setLog((log) => [...log, { text: message, type: 'from' }]);
 
-    clearTimeout(timeoutRef.current);
+    // отправка сообщения начинает все таймеры заново
+    clearTimeout(typingTimeoutRef.current);
+    clearTimeout(messageTimeoutRef.current);
 
-    timeoutRef.current = setTimeout(() => {
-      setLog((log) => [...log, { text: getSeniorMessage(), type: 'to' }]);
-    }, 1000 + Math.floor(Math.random() * 3000));
+    // интервал между отправкой сообщения и началом печати ответа
+    typingTimeoutRef.current = setTimeout(() => {
+      startTyping();
+    }, 500 + Math.floor(Math.random() * 1500));
   }, []);
 
   useEffect(() => {
-    return () => clearTimeout(timeoutRef.current);
+    return () => {
+      clearTimeout(typingTimeoutRef.current);
+      clearTimeout(messageTimeoutRef.current);
+    };
   }, []);
 
   return (
     <div className="grow flex flex-col">
-      <div className="grow overflow-auto flex flex-col justify-end p-4 gap-4">
-        {log.length ? (
-          log.map(({ text, type }, index) => (
-            <Message text={text} type={type} key={`${text}-${index}`} />
-          ))
-        ) : (
-          <p>Сформулируйте проблему максимально подробно и точно</p>
-        )}
+      <div className="grow overflow-auto flex flex-col justify-end p-4 pb-2 gap-4">
+        <p className="px-3 py-2 bg-gray-100 rounded-xl text-sm self-center">
+          Мы подобрали вам специалиста. Его зовут <b>Егор</b>. Сформулируйте проблему максимально
+          подробно и точно. Не переживайте, всё строго конфиденциально.
+        </p>
+
+        {log.map(({ text, type }, index) => (
+          <Message text={text} type={type} key={`${text}-${index}`} />
+        ))}
+      </div>
+      <div className="text-xs text-gray-400 px-4 pb-2">
+        {isTyping ? 'Егор набирает сообщение…' : '\u00A0'}
       </div>
       <div className="border-t-2">
         <Input onSubmit={handleSubmit} />
--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -10,10 +10,19 @@ export const IndexScreen = memo<Props>(({ onStartClick }) => {
     <div className="grow flex flex-col justify-center items-center text-center p-8">
       <img src={logo} style={{ width: 64, height: 64 }} className="mb-2 rounded-full" />
       <h1 className="text-xl font-semibold mb-2">Senior Duck</h1>
-      <p className="mb-2">Профессиональная помощь консультантов уровня senior и выше</p>
-      <p className="mb-8">Первая консультация бесплатна</p>
+      <p className="font-semibold mb-4">Бесплатные консультации с ведущими разработчиками онлайн</p>
+      <p className="mb-6">Регулярные сессии с опытными разработчиками повышают качество кода</p>
+      <ul className="mb-8 list-disc text-left p-4 pl-10 bg-gray-100 rounded-xl">
+        <li>Наладить гармоничные отношения с другими разработчиками</li>
+        <li>Справиться с тревогой и почувствовать спокойствие</li>
+        <li>Принять себя и повысить свою самооценку</li>
+        <li>Получить заряд энергии и перестать прокрастинировать</li>
+        <li>Найти своё призвание и начать заниматься любимым делом</li>
+        <li>Научиться говорить «Нет» и отстаивать свои границы</li>
+      </ul>
+
       <button onClick={onStartClick} className="bg-indigo-500 text-white px-4 py-2 rounded-full">
-        Начать
+        Хочу обсудить свою ситуацию
       </button>
     </div>
   );
